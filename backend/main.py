7390eafa8d4955b5da5a4ca52ad054bd4b9c8a66
iDhanush <idhanushm@gmail.com>
2024-12-02 01:13:10 +0530

    uvicorn setup

--- backend/main.py
+++ backend/main.py
@@ -1,1 +1,8 @@
+from frontcom.api import frontcom_router
+from server import app
 
+app.include_router(frontcom_router)
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="localhost", port=3987, reload=True)
