a47b512cc775c4a83ed07f8f91a4c4823da096c2
Abhinav K E <109943270+Abhinav-K-E@users.noreply.github.com>
2024-12-01 21:01:19 +0530

    nav added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,9 +2,9 @@ import './App.css'
 import Hero from './components/Hero/Hero'
 function App() {
   return (
-    <>
+    <div className='wrapper'>
       <Hero/>
-    </>
+    </div>
   )
 }
 
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,12 +3,16 @@ import App from "./App.jsx";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import { Toaster } from "react-hot-toast";
+import ChatPage from "./pages/Chat/ChatPage.jsx";
+import Nav from "./components/Nav/Nav.jsx";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
+    <Nav/>
     <Toaster />
     <Routes>
       <Route path="/" element={<App />} />
+      <Route path="/chat" element={<ChatPage />} />
     </Routes>
   </BrowserRouter>
 );
